86d2b720a73e3885fd59d1832d6d06c051084883
gaurav843 <gauravgarg843@gmail.com>
2021-01-04 15:58:48 +0530

    Initial

---
+++ StringCalculator/src/com/stringcalculator/StringCalculator.java
@@ -0,0 +1,31 @@
+package com.stringcalculator;
+
+import java.util.Scanner;
+
+public class StringCalculator {
+
+	public static void main(String arg[]) {
+
+		Scanner in = new Scanner(System.in);
+
+		String str = in.nextLine();
+
+		StringCalculator calculator = new StringCalculator();
+
+		System.out.print(calculator.Add(str));
+	}
+
+	int Add(String numbers) {
+		if (numbers.length() <= 0) {
+			return 0;
+		}
+		String[] numbersArray = numbers.split(",");
+		int total = 0;
+		for (int i = 0; i < numbersArray.length; i++) {
+			int num = Integer.parseInt(numbersArray[i]);
+			total = total + num;
+		}
+		return total;
+	}
+
+}
